6f4f373b4dce1a989171f1cf06ef86da4bef1087
bore <ibrakhimov2005@gmail.com>
2023-05-03 16:52:07 +0600

    register

---
+++ src/pages/Login/LoginPage.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../../context/AuthContextProvider";
+// import Loader from "../loader/Loader";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { handleLogin, error, setError, loading } = useAuth();
+
+  function handleSave(e) {
+    e.preventDefault();
+    if (!email.trim() || !password.trim()) {
+      alert("заполнете все поля");
+    } else {
+      let formData = new FormData();
+      formData.append("email", email);
+      formData.append("password", password);
+      handleLogin(formData, email);
+    }
+  }
+
+  useEffect(() => {
+    setError(false);
+  }, []);
+
+  // if (loading) {
+  //   return <Loader />;
+  // }
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      {error ? <h2>{error}</h2> : null}
+      <form action="submit" onSubmit={handleSave}>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="email"
+        />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="password"
+        />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,94 @@
+import "./Register.scss";
+
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../../context/AuthContextProvider";
+
+const Register = () => {
+  const auth = useAuth();
+  const [last_name, setFirstName] = useState("");
+  const [first_name, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  // const [errorMessage, setErrorMessage] = useState("");
+  // const [successMessage, setSuccessMessage] = useState(false);
+
+  const { handleRegister, error, loading, setError } = auth ?? {};
+  console.log(handleRegister);
+  function handleSave(e) {
+    e.preventDefault();
+    if (
+      !last_name.trim() ||
+      !first_name.trim() ||
+      !email.trim() ||
+      !password.trim() ||
+      !passwordConfirm.trim()
+    ) {
+      alert("заполните все поля");
+    } else {
+      let formData = new FormData();
+      formData.append("last_name", last_name);
+      formData.append("first_name", first_name);
+      formData.append("email", email);
+      formData.append("password", password);
+      formData.append("password2", passwordConfirm);
+      handleRegister(formData);
+    }
+  }
+
+  useEffect(() => {
+    setError(false);
+  }, []);
+
+  return (
+    <div>
+      <h1>Register</h1>
+      {error ? <h2>{error}</h2> : null}
+      <form onSubmit={handleSave}>
+        <label>
+          Имя:
+          <input
+            onChange={(e) => setFirstName(e.target.value)}
+            type="text"
+            placeholder="first name"
+          />
+        </label>
+        <label>
+          Фамилия:
+          <input
+            onChange={(e) => setLastName(e.target.value)}
+            type="text"
+            placeholder="last name"
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="email"
+          />
+        </label>
+        <label>
+          Пароль:
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="password"
+          />
+        </label>
+        <label>
+          Подтверждение пароля:
+          <input
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+            type="password"
+            placeholder="confirm password"
+          />
+        </label>
+        <button type="submit">Зарегистрироваться</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
